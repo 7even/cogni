62b8470eeffbd4aa93c242186e87edf3e0678681
Vsevolod Romashov <7@7vn.ru>
2019-05-13 00:19:52 +0300

    remove redundant :db.install/_attribute

--- src/cogni/db.clj
+++ src/cogni/db.clj
@@ -14,7 +14,6 @@
     :db/ident :purchase/name
     :db/valueType :db.type/string
     :db/cardinality :db.cardinality/one
-    :db/doc "The name of the purchase"
-    :db.install/_attribute :db.part/db}])
+    :db/doc "The name of the purchase"}])
 
 (d/transact conn schema)
