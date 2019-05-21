4d1e2805eca97b13b102ae6abec29e836707f395
Vsevolod Romashov <7@7vn.ru>
2019-05-21 03:23:48 +0300

    remove obsolete code, refactor handlers

--- src/cogni/http.clj
+++ src/cogni/http.clj
@@ -1,13 +1,8 @@
 (ns cogni.http
-  (:require [clojure.edn :as edn]
-            [cogni.db :as db]
+  (:require [cogni.db :as db]
             [io.pedestal.http :as http]
             [io.pedestal.http.body-params :refer [body-params]]
             [io.pedestal.http.route :as route]))
-
-(defn hello-world [request]
-  {:status 200
-   :body "Hello, World!"})
 
 (defn list-purchases [{:keys [::db/value]}]
   (->> (db/get-purchases value)
@@ -17,18 +12,19 @@
                :added-at added-at}))
        http/edn-response))
 
-(defn add-purchase [{:keys [::db/conn] {:keys [name]} :edn-params}]
+(defn add-purchase [{:keys [::db/conn]
+                     {:keys [name]} :edn-params}]
   (db/add-purchase conn name)
   {:status 201})
 
-(defn retract-purchase [{:keys [::db/conn] {:keys [name]} :path-params}]
+(defn retract-purchase [{:keys [::db/conn]
+                         {:keys [name]} :path-params}]
   (db/retract-purchase conn name)
   {:status 204})
 
 (defn routes [db]
   (route/expand-routes
-   #{["/hello" :get hello-world :route-name :hello]
-     ["/purchases" :get [(db/attach-database db) list-purchases] :route-name :purchases]
+   #{["/purchases" :get [(db/attach-database db) list-purchases] :route-name :purchases]
      ["/purchases" :post [(body-params) (db/attach-database db) add-purchase] :route-name :add-purchase]
      ["/purchases/:name" :delete [(db/attach-database db) retract-purchase] :route-name :retract-purchase]}))
 
