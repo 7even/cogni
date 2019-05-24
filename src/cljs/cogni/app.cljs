96ce3d6a0c9fd4046abc1ff3bae88c309328aadb
Vsevolod Romashov <7@7vn.ru>
2019-05-25 01:10:31 +0300

    load purchases from backend after loading the page

--- src/cljs/cogni/app.cljs
+++ src/cljs/cogni/app.cljs
@@ -1,12 +1,30 @@
 (ns cogni.app
   (:require [reagent.core :as ra]
-            [re-frame.core :as rf]))
+            [re-frame.core :as rf]
+            [day8.re-frame.http-fx]
+            [ajax.edn :as ajax]))
 
 (rf/reg-event-db :initialize-db
                  (fn [_ _]
-                   {:purchases ["Bread"
-                                "Milk"
-                                "Beer"]}))
+                   {:purchases []}))
+
+(rf/reg-event-fx :load-purchases
+                 (fn [{db :db} _]
+                   {:http-xhrio {:method :get
+                                 :uri "http://localhost:8890/purchases"
+                                 :format (ajax/edn-request-format)
+                                 :response-format (ajax/edn-response-format)
+                                 :on-success [:purchases-loaded]
+                                 :on-failure [:purchases-failed-to-load]}}))
+
+(rf/reg-event-db :purchases-loaded
+                 (fn [db [_ response]]
+                   (assoc db :purchases (map :name response))))
+
+(rf/reg-event-db :purchases-failed-to-load
+                 (fn [db [_ response]]
+                   (println "Failed to load purchases:" response)
+                   db))
 
 (rf/reg-sub :purchases
             (fn [db]
@@ -24,4 +42,5 @@
 
 (defn init []
   (rf/dispatch-sync [:initialize-db])
+  (rf/dispatch-sync [:load-purchases])
   (render))
