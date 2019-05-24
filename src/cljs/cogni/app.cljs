dcec46a4aff5c90bd2019843e786aefcc6702135
Vsevolod Romashov <7@7vn.ru>
2019-05-24 03:55:18 +0300

    add a basic re-frame app

--- src/cljs/cogni/app.cljs
+++ src/cljs/cogni/app.cljs
@@ -1,8 +1,27 @@
-(ns cogni.app)
+(ns cogni.app
+  (:require [reagent.core :as ra]
+            [re-frame.core :as rf]))
+
+(rf/reg-event-db :initialize-db
+                 (fn [_ _]
+                   {:purchases ["Bread"
+                                "Milk"
+                                "Beer"]}))
+
+(rf/reg-sub :purchases
+            (fn [db]
+              (:purchases db)))
+
+(defn ui []
+  [:div
+   [:h2 "Purchases"]
+   [:ul
+    (for [purchase @(rf/subscribe [:purchases])]
+      [:li purchase])]])
 
 (defn render []
-  (println "render"))
+  (ra/render [ui] (js/document.getElementById "root")))
 
 (defn init []
-  (println "init")
+  (rf/dispatch-sync [:initialize-db])
   (render))
