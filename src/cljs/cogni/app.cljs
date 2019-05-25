a59be6f3cabe0975122c4ff37667e3565a0b4d9d
Vsevolod Romashov <7@7vn.ru>
2019-05-25 21:56:34 +0300

    add basic Add purchase to the UI

--- src/cljs/cogni/app.cljs
+++ src/cljs/cogni/app.cljs
@@ -7,6 +7,7 @@
 (rf/reg-event-db :initialize-db
                  (fn [_ _]
                    {:purchases []
+                    :new-purchase ""
                     :loading? true
                     :loading-error nil}))
 
@@ -22,7 +23,7 @@
 (rf/reg-event-db :purchases-loaded
                  (fn [db [_ response]]
                    (-> db
-                       (assoc :purchases (map :name response))
+                       (assoc :purchases (mapv :name response))
                        (assoc :loading? false))))
 
 (rf/reg-event-db :purchases-failed-to-load
@@ -31,9 +32,23 @@
                        (assoc :loading-error (str "Failed to load purchases: " response))
                        (assoc :loading? false))))
 
+(rf/reg-event-db :change-new-purchase
+                 (fn [db [_ new-purchase]]
+                   (assoc db :new-purchase new-purchase)))
+
+(rf/reg-event-db :add-purchase
+                 (fn [db _]
+                   (-> db
+                       (update :purchases conj (:new-purchase db))
+                       (assoc :new-purchase ""))))
+
 (rf/reg-sub :purchases
             (fn [db]
               (:purchases db)))
+
+(rf/reg-sub :new-purchase
+            (fn [db]
+              (:new-purchase db)))
 
 (rf/reg-sub :loading?
             (fn [db]
@@ -56,7 +71,12 @@
      [:h2 "Purchases"]
      [:ul
       (for [purchase @(rf/subscribe [:purchases])]
-        [:li {:key purchase} purchase])]]))
+        [:li {:key purchase} purchase])]
+     [:input {:type :text
+              :value @(rf/subscribe [:new-purchase])
+              :on-change #(rf/dispatch [:change-new-purchase
+                                        (-> % .-target .-value)])}]
+     [:button {:on-click #(rf/dispatch [:add-purchase])} "Add"]]))
 
 (defn render []
   (ra/render [ui] (js/document.getElementById "root")))
